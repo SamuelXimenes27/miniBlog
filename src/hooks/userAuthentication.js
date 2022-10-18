9333ea46cbccd231f2b7b1a0cf02f4648d43c322
SamuelXimenes27 <samuel.ximenes.oliveira60@aluno.ifce.edu.br>
2022-10-18 18:00:13 -0300

    Login flux and authentication created

--- src/hooks/userAuthentication.js
+++ src/hooks/userAuthentication.js
@@ -77,6 +77,36 @@ export const useAuthentication = () => {
         signOut(auth);
     }
 
+    // login - sign in
+    const login = async (data) => {
+
+        checkIfCancelled();
+
+        setLoading(true);
+        setError(false);
+
+        try {
+
+            await signInWithEmailAndPassword(auth, data.email, data.password);
+            setLoading(false);
+
+        } catch (error) {
+
+            let systemErrorMessage;
+
+            if (error.message.includes("user-not-found")) {
+                systemErrorMessage = "Usuario nao encontrado."
+            } else if (error.message.includes("wrong-password")) {
+                systemErrorMessage = "Senha incorreta."
+            } else {
+                systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde."
+            }
+
+            setError(systemErrorMessage);
+            setLoading(false);
+        }
+    }
+
     useEffect(() => {
         return () => setCancelled(true);
     }, []);
@@ -87,5 +117,6 @@ export const useAuthentication = () => {
         error,
         loading,
         logout,
+        login,
     };
 }
